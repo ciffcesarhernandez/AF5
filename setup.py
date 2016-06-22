44f2b32daef28faa24859f95395ff17514e21e4c
César <cesarhernandez@campusciff.net>
2016-06-22 17:13:38 +0200

    Correccion setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['prusontchm'], # this must be the same as the name above
   version = '1.1',
   description = 'practica3',
-  author = 'Cesar Hernandez y Sara Otero de Navascués', 
+  author = 'Cesar Hernandez y Sara Otero de Navascues', 
   author_email = 'cesarhernandez@campusciff.net, saraotero@campusciff',
   url = 'https://github.com/ciffcesarhernandez/practica3', # use the URL to the github repo
   download_url = 'https://github.com/ciffcesarhernandez/practica3/tarball/1.1',
