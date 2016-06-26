90f86b6bb9f393a46db939f0ffe529070cdd2e63
ciffcesarhernandez <cesarhernandez@campusciff.net>
2016-06-26 13:30:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'prusontchm',
   packages = ['prusontchm'], # this must be the same as the name above
-  version = '2.0',
+  version = '2.1',
   description = 'practica3',
   author = 'Cesar Hernandez y Sara Otero de Navascues', 
   author_email = 'cesarhernandez@campusciff.net, saraotero@campusciff',
   url = 'https://github.com/ciffcesarhernandez/practica3', # use the URL to the github repo
-  download_url = 'https://github.com/ciffcesarhernandez/practica3/tarball/2.0',
+  download_url = 'https://github.com/ciffcesarhernandez/practica3/tarball/2.1',
   keywords = ['testing', 'logging', 'example','ciff'],
   classifiers = [],
 )
